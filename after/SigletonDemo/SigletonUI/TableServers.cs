56716e5e25acf80fbd2aa76f3e314231a985c710
Tarek Inoubli <tarekinoubli@gmail.com>
2022-01-28 11:01:54 +0100

    Add Solution using Singleton

---
+++ after/SigletonDemo/SigletonUI/TableServers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigletonUI
+{
+    public class TableServers
+    {
+        private static readonly TableServers _instance = new TableServers();
+        private List<string> _servers = new List<string>();
+        private int nextServer = 0;
+
+        public TableServers()
+        {
+            _servers.Add("Tim");
+            _servers.Add("Sue");
+            _servers.Add("Mary");
+            _servers.Add("Bob");
+        }
+
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+
+        public string GetNextServer()
+        {
+            string output = _servers[nextServer];
+
+            nextServer++;
+
+            if (nextServer >= _servers.Count)
+            {
+                nextServer = 0;
+            }
+
+            return output;
+        }
+    }
+}
