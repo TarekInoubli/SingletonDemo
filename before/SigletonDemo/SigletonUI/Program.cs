7ea5e3c476ac2032af27ba72ca33e56ce025773c
Tarek Inoubli <tarekinoubli@gmail.com>
2022-01-28 10:56:46 +0100

    Add Solution without using Singleton

---
+++ before/SigletonDemo/SigletonUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using SigletonUI;
+
+class Program
+{
+    static TableServers host1List = new TableServers();
+    static TableServers host2List = new TableServers();
+
+    static void Main(string[] args)
+    {
+        TableServers servers = new TableServers();
+
+        for (int i = 0; i < 10; i++)
+        {
+            //Console.WriteLine($"The next server is: {servers.GetNextServer()}");
+
+            Host1GetNextServer();
+            Host2GetNextServer();
+        }
+
+        Console.ReadLine();
+    }
+
+    private static void Host1GetNextServer()
+    {
+        Console.WriteLine($"The next server is: {host1List.GetNextServer()}");
+    }
+
+    private static void Host2GetNextServer()
+    {
+        Console.WriteLine($"The next server is: {host2List.GetNextServer()}");
+    }
+}
---
+++ before/SigletonDemo/SigletonUI/TableServers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigletonUI
+{
+    public class TableServers
+    {
+        private List<string> _servers = new List<string>();
+        private int nextServer = 0;
+
+        public TableServers()
+        {
+            _servers.Add("Tim");
+            _servers.Add("Sue");
+            _servers.Add("Mary");
+            _servers.Add("Bob");
+        }
+
+        public string GetNextServer()
+        {
+            string output = _servers[nextServer];
+
+            nextServer++;
+
+            if (nextServer >= _servers.Count)
+            {
+                nextServer = 0;
+            }
+
+            return output;
+        }
+    }
+}
